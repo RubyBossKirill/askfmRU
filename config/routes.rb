ef6e6338381f335d3771a2b0ec5bbcb9dc864136
RubyBossKirill <160587634+RubyBossKirill@users.noreply.github.com>
2024-07-10 12:36:43 +0500

    feat: settings show questions for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :questions
-  resources :users, :question, except: [:destroy]
+  resources :users, :questions, except: [:destroy]
 
   root "users#index"
 end
