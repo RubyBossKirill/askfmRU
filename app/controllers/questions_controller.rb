6001e4f1997e4bb7fdb5a56b296a3aca5db6da1b
RubyBossKirill <160587634+RubyBossKirill@users.noreply.github.com>
2024-07-10 11:25:48 +0500

    feat: settings questions_controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,52 @@
+class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[ show edit update ]
+
+  # GET /questions
+  def index
+    @questions = Question.all
+  end
+
+  # GET /questions/1
+  def show
+  end
+
+  # GET /questions/new
+  def new
+    @question = Question.new
+  end
+
+  # GET /questions/1/edit
+  def edit
+  end
+
+  # POST /questions
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @user, notice: "Question was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /questions/1
+  def update
+    if @question.update(question_params)
+      redirect_to @user, notice: "Question was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def question_params
+      params.require(:question).permit(:title, :body, :user_id)
+    end
+end
