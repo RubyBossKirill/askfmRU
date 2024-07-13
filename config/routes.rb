cab5a38eaaca5f702933ef2aa0a759707fcd4349
RubyBossKirill <160587634+RubyBossKirill@users.noreply.github.com>
2024-07-13 17:12:50 +0500

    feat: settings methods get in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get "sign_up" => "users#new"
+  get "log_out" => "sessions#destroy"
+  get "log_in" => "sessions#new"
+
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, except: [:destroy] do
     resources :questions, except: [:show, :edit, :update], on: :collection
   end
