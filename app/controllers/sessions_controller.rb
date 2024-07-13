530cbeab24f4483492eb113588a2725eb4e88fdf
RubyBossKirill <160587634+RubyBossKirill@users.noreply.github.com>
2024-07-13 18:31:39 +0500

    feat: creat helper_method :current_user. Settings sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.authenticate(params[:email], params[:password])
+
+    if @user.present?
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Вы успешно вошли"
+    else
+      redirect_to log_in_path, notice: "Неверный логин или пароль"
+    end
   end
 
   def destroy
