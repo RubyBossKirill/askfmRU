a1554f1495a8936eca112e74ab728de87948761c
RubyBossKirill <160587634+RubyBossKirill@users.noreply.github.com>
2024-07-13 16:58:15 +0500

    feat: install sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users, except: [:destroy] do
     resources :questions, except: [:show, :edit, :update], on: :collection
   end
