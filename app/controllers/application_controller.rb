8acbdec95fd57bfaeadbc5dbece7e35b6fbb0c13
RubyBossKirill <160587634+RubyBossKirill@users.noreply.github.com>
2024-07-16 13:12:37 +0500

    fix: COLOR question and add random

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,7 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :random_color
+  helper_method :current_user
 
   private
-  def random_color
-    [
-      ['Красный', 'rgba(255, 87, 51, 0.2)'],
-      ['Зелёный', 'rgba(51, 255, 87, 0.2)'],
-      ['Голубой', 'rgba(51, 87, 255, 0.2)'],
-      ['Розовый', 'rgba(255, 51, 161, 0.2)'],
-      ['Оранжевый', 'rgba(255, 191, 51, 0.2)']
-    ].sort_by{ rand }
-  end
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,9 @@ class Question < ApplicationRecord
     belongs_to :user
     validates :body, presence: true
     validates :title, length: { maximum: 124 }
+    COLOR = [['Красный', 'rgba(255, 87, 51, 0.7)'],
+    ['Зелёный', 'rgba(51, 255, 87, 0.7)'],
+    ['Голубой', 'rgba(51, 87, 255, 0.7)'],
+    ['Розовый', 'rgba(255, 51, 161, 0.7)'],
+    ['Оранжевый', 'rgba(255, 191, 51, 0.7)']]
 end
