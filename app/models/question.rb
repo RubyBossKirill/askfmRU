f596c60de63f2306f838561c5613a4fcebfc44df
RubyBossKirill <160587634+RubyBossKirill@users.noreply.github.com>
2024-07-18 15:38:48 +0500

    fix: rename COLOR -> COLORS. And add method .shuffle in form

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
     belongs_to :user
     validates :body, presence: true
     validates :title, length: { maximum: 124 }
-    COLOR = [['Красный', 'rgba(255, 87, 51, 0.7)'],
+    COLORS = [['Красный', 'rgba(255, 87, 51, 0.7)'],
     ['Зелёный', 'rgba(51, 255, 87, 0.7)'],
     ['Голубой', 'rgba(51, 87, 255, 0.7)'],
     ['Розовый', 'rgba(255, 51, 161, 0.7)'],
